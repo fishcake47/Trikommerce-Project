482661eeec6616a2a4ac31639cc6d4b317d99778
fishcake47 <mason.chinery@gmail.com>
2024-10-01 15:51:53 +0100

    Fixed the code such that the day 0 will have 2000 items restocked instead of 0

--- IMS_simulation.py
+++ IMS_simulation.py
@@ -2,7 +2,7 @@ from restock import restock_inventory
 from sales import daily_sales
 
 def run_simulation(total_days, inventory_records):
-    available_items = 2000
+    available_items = 0
     for current_day in range(0, total_days):
         available_items = restock_inventory(available_items, inventory_records, current_day)
         available_items = daily_sales(available_items, inventory_records, current_day)
